7060d0172ab6112ee4729bd6dd740c43e6d13f67
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-24 13:47:51 +0100

    Add files

---
+++ include/Ishiko/Memory/Buffer.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/memory/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_MEMORY_BUFFER_HPP_
+#define _ISHIKO_CPP_MEMORY_BUFFER_HPP_
+
+#endif
---
+++ include/Ishiko/Memory/linkoptions.hpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/memory/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_MEMORY_LINKOPTIONS_HPP_
+#define _ISHIKO_CPP_MEMORY_LINKOPTIONS_HPP_
+
+#ifdef _DEBUG
+#pragma comment(lib, "IshikoMemory-d.lib")
+#else
+#pragma comment(lib, "IshikoMemory.lib")
+#endif
+
+#endif
