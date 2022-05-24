b22c0fa3af001f7d7ee6639ce561169e8663f41e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-24 14:46:47 +0100

    Add Buffer class (#1)

---
+++ include/Ishiko/Memory.hpp
@@ -0,0 +1,13 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/memory/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_MEMORY_HPP_
+#define _ISHIKO_CPP_MEMORY_HPP_
+
+#include "Memory/Buffer.hpp"
+#include "Memory/linkoptions.hpp"
+
+#endif
---
+++ include/Ishiko/Memory/Buffer.hpp
@@ -0,0 +1,21 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/memory/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_MEMORY_BUFFER_HPP_
+#define _ISHIKO_CPP_MEMORY_BUFFER_HPP_
+
+namespace Ishiko
+{
+
+// TODO: this class is used to store raw bytes. For instance the buffer could be reallocatd and the contents moved with
+// a memcpy or similar operations.
+class Buffer
+{
+};
+
+}
+
+#endif
